00662c286f9c09d8c977f86d738d1ffe45be3303
TooAndy <aniss@qq.com>
2022-05-28 22:22:51 +0800

    init

---
+++ src/test/java/fun/aniss/netty/c1/TestByteBufferReadWrite.java
@@ -0,0 +1,24 @@
+package fun.aniss.netty.c1;
+
+import java.nio.ByteBuffer;
+
+import static fun.aniss.netty.c1.ByteBufferUtil.debugAll;
+import static fun.aniss.netty.c1.ByteBufferUtil.debugRead;
+
+/**
+ * @author TooAndy
+ * date 2022/5/23 23:00
+ */
+public class TestByteBufferReadWrite {
+    public static void main(String[] args) {
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+
+        buffer.put((byte) 0x61);
+        debugAll(buffer);
+        buffer.put(new byte[]{0x62, 0x63, 0x64});
+        debugAll(buffer);
+        buffer.flip();
+        byte b = buffer.get();
+        debugRead(buffer);
+    }
+}
